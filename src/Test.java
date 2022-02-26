a3993341e6f4540f914339a4ad2ad4a7c83b17df
praneeth6660 <praneeth.bandaru123@gmail.com>
2022-02-26 16:07:08 +0530

    First Commit.....

---
+++ src/Test.java
@@ -0,0 +1,61 @@
+
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Timer;
+import java.util.zip.Deflater;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+
+import org.json.JSONObject;
+
+import sun.misc.BASE64Decoder;
+
+public class Test {
+
+	/*public static void main(String[] args){
+	       TimerExample te1=new TimerExample("Task1");
+	       TimerExample te2=new TimerExample("Task2");
+	      Timer t=new Timer();
+	      t.scheduleAtFixedRate(te1, 0,10*1000);
+	      t.scheduleAtFixedRate(te2, 0,1000);
+	   }*/
+	
+	
+	 public static void main(String[] args) {
+		// byte[] CDRIVES = hexStringToByteArray("e04fd020ea3a6910a2d808002b30309d");
+		    
+	     System.out.println("Hello World");
+	 }   
+	    
+	    
+	    
+	    
+}
